3cd651c6bc9e1cdee6776c3e8eca570e11170243
atharvishal <bcsf15a548@pucit.edu.pk>
2018-10-07 13:00:32 -0700

    I editted the task1-2 word file

--- task2.c
+++ task2.c
@@ -3,62 +3,41 @@
 #include <unistd.h>
 #include <signal.h>
 int global_sum = 0;
-int main(int argc, char *argv[]) {
-	int level = 0;
-	int fd[2];
+int main() {
+	int count = 1;
+	int c = 1;
 	int sum = 0;
+	int cpid = -1;
 	
-	// Just a dummy Array
+	// Array
 	int array[1000];
 	// Initializing Array
-	for (int i = 0; i < 1000; ++i)
+	for (int i = 0; i < 1000; i++)
 	{
 		array[i] = i;
 	}
 
-	for (int j = 0; j < 1000; ++j)
+	for (int i = 0; i < 10; i++)
 	{	
-		if(j%100 == 0)
+		cpid = fork()
+		if (cpid == 0)
 		{
-			// create pipe descriptors
-			pipe(fd);
-			level++;
-			sum = 0;
-			
-			// fork() returns 0 for child process, child-pid for parent process.
-			if (fork() == 0) {
-				// Child
-				close(fd[0]);
-				for (int i = j; i < j+100; ++i)
-				{
-					sum += array[i];
-				}	
-					
-				write(fd[1], &sum, sizeof(sum));
-				
-				printf("Chile send %d Sum at %d Level\n", sum, level);
-				
-				// close the write descriptor
-				close(fd[1]);
-				kill(getpid(), SIGKILL);
-			} 
-			else 
+			for(j=count; j<c *100;j++)
 			{
-				// Parent: reading only, so close the write-descriptor
-				close(fd[1]);
-				wait(NULL);
-
-				// now read the data (will block until it succeeds)
-				read(fd[0], &sum, sizeof(sum));
-				global_sum = global_sum + sum;
-				
-				printf("Parent received %d Sum at %d Level\n\n", sum, level);
-
-				// close the read-descriptor
-				close(fd[0]);
+				sum += array[i];
+				count++;
 			}
 		}
+		else
+		{
+			wait(NULL);
+			global_sum = global_sum + sum;
+		}
+		
+		c+=1;
+		sum = 0;
 	}
+	
 	printf("\nGlobal Sum = %d\n", global_sum);
 
 	return 0;
